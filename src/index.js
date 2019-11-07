d00fd3f371ecb7b21a5ce9b37c006561f77b7f63
Kirill Chertkov <chertkov.k@gmail.com>
2019-11-07 20:04:56 +0300

    add user's name request

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import readlineSync from 'readline-sync';
+
+const greetings = (name) => {
+  console.log(`Hello, ${name}`);
+};
+
+const interaction = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  greetings(name);
+};
+
+export { interaction };
