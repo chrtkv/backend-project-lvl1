a31b51b72a593dec72605a29f3050ab979bda380
Kirill Chertkov <chertkov.k@gmail.com>
2019-11-07 22:28:41 +0300

    change script structure

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import interaction from '..';
+import game from '..';
 
-interaction();
+game();
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import readlineSync from 'readline-sync';
 
+// welcome the user
 const greetings = (name) => {
   console.log(`Hello, ${name}`);
 };
 
+// interact with the user
 const interaction = () => {
-  console.log('Welcome to the Brain Games!');
+  // get user name
   const name = readlineSync.question('May I have your name? ');
   greetings(name);
 };
 
-export { interaction as default };
+// main function
+const game = () => {
+  console.log('Welcome to the Brain Games!');
+  interaction();
+};
+
+export default game;
