d1ccfcf71728cfe2e3786e2d320443f08352f2c3
Kirill Chertkov <chertkov.k@gmail.com>
2019-11-27 17:50:41 +0300

    apply mentor's remarks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import readlineSync from 'readline-sync';
 
-// set number of rounds
 const attempts = 3;
 
 const engine = (gameFunction, description) => {
@@ -9,12 +8,11 @@ const engine = (gameFunction, description) => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!\n`);
 
-  // start the game loop
   for (let i = attempts; i > 0; i -= 1) {
     const { question, answer } = gameFunction();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    // check the round results
+
     if (answer !== userAnswer) {
       console.log(`\n"${userAnswer}" is wrong answer. Correct answer was "${answer}".\n`);
       console.log(`Let's try again, ${name}!\n`);
@@ -22,7 +20,6 @@ const engine = (gameFunction, description) => {
     }
     console.log('Correct!\n');
   }
-  // print final message
   console.log(`Congratulations, ${name}!\n`);
 };
 
